4086f8460130dd5842efefd468c7dc920649f81e
Sviatoslav Kozar <slav@kozar.xyz>
2026-07-27 16:58:17 -0600

    debug: print staging contents

--- Formula/keepalive-cli.rb
+++ Formula/keepalive-cli.rb
@@ -6,8 +6,18 @@ class KeepaliveCli < Formula
   sha256 "b415dd1b6ed9016f0709b13d24af01bf5605f7376cc860f5a7630d8fc4f9f1fb"
 
   def install
+    # Debug: list what's in the staging directory
+    ohai "Staging directory contents: #{Dir['*'].inspect}"
     libexec.install Dir["*"]
-    FileUtils.mv(libexec/"keepalive-app-bundle", libexec/"keepalive-cli.app")
+    ohai "Libexec contents: #{(libexec.children).inspect}"
+    
+    # Rename to .app bundle
+    src = libexec/"keepalive-app-bundle"
+    dst = libexec/"keepalive-cli.app"
+    ohai "Renaming #{src} to #{dst}"
+    ohai "Source exists: #{src.exist?}"
+    FileUtils.mv(src, dst) if src.exist?
+    
     bin.install_symlink libexec/"keepalive-cli.app/Contents/MacOS/keepalive-cli" => "keepalive-cli"
   end
 end
