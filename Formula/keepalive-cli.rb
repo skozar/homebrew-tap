617a1071eb82b8961ebe5ea533ca86d79370bbd3
Sviatoslav Kozar <slav@kozar.xyz>
2026-07-27 17:08:57 -0600

    test: debug CWD

--- Formula/keepalive-cli.rb
+++ Formula/keepalive-cli.rb
@@ -6,11 +6,28 @@ class KeepaliveCli < Formula
   sha256 "4f6be1d10fc632671f9dab091cc4075bda753db4c192fca167dee04e966f2b45"
 
   def install
+    ohai "CWD: #{Dir.pwd}"
+    ohai "Dir entries: #{Dir['*'].inspect}"
+    
     app = libexec/"keepalive-cli.app"
     (app/"Contents").mkpath
-    Pathname("Contents").children.each do |child|
-      FileUtils.mv(child, app/"Contents")
+    
+    # Find the Contents directory (could be at root of staging)
+    contents_dir = Dir.children(".").find { |f| f == "Contents" }
+    ohai "Contents dir found: #{contents_dir}"
+    
+    if contents_dir
+      FileUtils.mv(Pathname.new(contents_dir).children, app/"Contents")
+    else
+      # Fallback: install everything and find Contents
+      libexec.install Dir["*"]
+      ohai "Libexec contents: #{libexec.children.inspect}"
+      src = libexec/"Contents"
+      if src.exist?
+        FileUtils.mv(src.children, app/"Contents")
+      end
     end
+    
     system "codesign", "--force", "--deep", "--sign", "-", app.to_s
     bin.install_symlink app/"Contents/MacOS/keepalive-cli" => "keepalive-cli"
   end
