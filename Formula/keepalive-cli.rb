aa7a7abaac12fcee9ce37b2f49de5ee902fec112
Sviatoslav Kozar <slav@kozar.xyz>
2026-07-27 17:08:10 -0600

    test: flat Contents tarball approach

--- Formula/keepalive-cli.rb
+++ Formula/keepalive-cli.rb
@@ -2,18 +2,16 @@ class KeepaliveCli < Formula
   desc "Keep macOS awake for Teams during chosen hours"
   homepage "https://github.com/skozar/keepalive"
   version "0.11.4"
-  url "file:///Users/skozar/Projects/pets/keepalive/dist/keepalive-flat2.tar.gz"
-  sha256 "0757546e7935e385559deda60ff1a2277c504601da0f7f534f9678ee345c2f8a"
+  url "file:///tmp/keepalive-test.tar.gz"
+  sha256 "4f6be1d10fc632671f9dab091cc4075bda753db4c192fca167dee04e966f2b45"
 
   def install
-    # Create .app bundle and install Contents/ into it
     app = libexec/"keepalive-cli.app"
-    app.mkpath
     (app/"Contents").mkpath
-    # Move extracted Contents/ items into .app/Contents/
-    contents_dir = Pathname("Contents")
-    FileUtils.mv(contents_dir.children, app/"Contents")
-    
+    Pathname("Contents").children.each do |child|
+      FileUtils.mv(child, app/"Contents")
+    end
+    system "codesign", "--force", "--deep", "--sign", "-", app.to_s
     bin.install_symlink app/"Contents/MacOS/keepalive-cli" => "keepalive-cli"
   end
 end
